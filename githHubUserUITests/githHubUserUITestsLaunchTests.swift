b6b852fb62d8dcc48701920be98472519de20663
abedir@youxel.com <@AEzzat75184285>
2024-05-31 02:10:23 +0300

    initial commit

---
+++ githHubUserUITests/githHubUserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  githHubUserUITestsLaunchTests.swift
+//  githHubUserUITests
+//
+//  Created by Ahmed Ezzat on 31/05/2024.
+//
+
+import XCTest
+
+final class githHubUserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
